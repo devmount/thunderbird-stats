d39594210fc2dc43d84375459a87cdb98ac817ae
Andreas Müller <mail@devmount.de>
2019-09-04 21:23:35 +0200

    :heavy_plus_sign: add first line chart

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<LineChart title="Mails per Year" :datasets="mailsPerYear.datasets" :labels="mailsPerYear.labels" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// internal components
+import LineChart from './components/LineChart.vue'
+
+// chart data
+import MAILS_PER_HOUR from './data/mails-per-hour.json'
+import MAILS_PER_MONTH from './data/mails-per-month.json'
+import MAILS_PER_YEAR from './data/mails-per-year.json'
+
+// initialize Chart.js default options
+import Chart from 'chart.js'
+Chart.defaults.global.elements.arc.borderWidth = 0;
+Chart.defaults.global.legend.display = false;
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		LineChart
+	},
+	stats: {
+		mailsPerHour: MAILS_PER_HOUR,
+		mailsPerMonth: MAILS_PER_MONTH,
+		mailsPerYear: MAILS_PER_YEAR,
+	},
+	computed: {
+		mailsPerYear () {
+			var din = this.$options.stats.mailsPerYear.in
+			var dout = this.$options.stats.mailsPerYear.out
+			var years = [], dsin = [], dsout = []
+			for (const year in din) {
+				years.push(year)
+				dsin.push(din[year])
+				dsout.push(dout[year])
+			}
+			return {
+				datasets: [
+					{ data: dsin,  color: 'rgb(48, 206, 242)' },
+					{ data: dsout, color: 'rgb(237, 47, 71)'  }
+				],
+				labels: years
+			}
+		}
+	},
 }
 </script>
 <style lang="stylus">
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+	font-family 'Avenir', Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+	text-align center
+	color #2c3e50
+	margin-top 60px
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+	<h3 class="text-center">{{ title }}</h3>
+	<canvas :id='id'></canvas>
+</div>
+</template>
+
+<script>
+/* eslint no-undef: 0 */
+export default {
+	props: {
+		title: String,
+		labels: Array,
+		datasets: Array,
+	},
+	data () {
+		return {
+			id: Math.random().toString(36).substring(7)
+		}
+	},
+	mounted () {
+		if (this.title != '' && this.labels && this.datasets) {
+			let datasets = []
+			for (let i = 0; i < this.datasets.length; i++) {
+				const dataset = this.datasets[i];
+				datasets.push({
+					data: dataset.data,
+					borderColor: dataset.color,
+					lineTension: 0.15,
+					pointBorderColor: 'rgb(0, 0, 0, 0)',
+					pointRadius: 15,
+					backgroundColor: 'rgb(0, 0, 0, 0)',
+				})
+			}
+			new Chart(this.id, {
+				type: 'line',
+				data: {
+					datasets: datasets,
+					labels: this.labels,
+				},
+			})
+		}
+	}
+}
+</script>
