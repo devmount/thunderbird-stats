248e8e41108c59ef79fd9a3753e14657f4b68bb9
Andreas Müller <mail@devmount.de>
2019-09-09 01:04:23 +0200

    :gear: handle more date formats

--- stats.py
+++ stats.py
@@ -32,16 +32,27 @@ def stats():
 				startindex = line.index(',')+2 if ',' in line else line.index(' ')+1
 				datestring = line[startindex:].strip()
 				datestring = datestring[:datestring.rindex(" ")] if len(datestring.split(" "))>5 else datestring
-				maildate = time.strptime(datestring, '%d %b %Y %H:%M:%S %z')
-			# if maildate was not found, try alternative approach
+				try:
+					maildate = time.strptime(datestring, '%d %b %Y %H:%M:%S %z')
+				except ValueError:
+					maildate = None
+			# if maildate was not found, try alternative approaches/formats
 			if maildate is None and ' +0000' in line and len(line)>26:
 				datestring = line[line.index(' +0000')-20:line.index(' +0000')+6]
 				try:
 					maildate = time.strptime(datestring, '%d %b %Y %H:%M:%S %z')
 				except ValueError:
-					maildate = None
+					datestring = line[8:26]
+					try:
+						maildate = time.strptime(datestring, '%Y-%m-%d %H:%M:%S')
+					except ValueError:
+						datestring = line[6:26]
+						try:
+							maildate = time.strptime(datestring, '%d %b %Y %H:%M:%S')
+						except ValueError:
+							maildate = None
 			# save found data
-			if line.startswith('Content-Length: ') and mailtype is not None and maildate is not None:
+			if mailtype is not None and maildate is not None:
 				# build mails per year and per month
 				if maildate.tm_year in mails_per_year[mailtype]:
 					mails_per_year[mailtype][maildate.tm_year] += 1
